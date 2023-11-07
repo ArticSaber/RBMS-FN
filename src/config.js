5c8acf47a4a4d4aaddc5a08da1fe00c7b72eea55
ArticSaber <kaarthiknirmala@gmail.com>
2023-11-08 00:50:20 +0530

    config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NEXT_PUBLIC_NODE_ENV === "development"
     ? "http://localhost:3000/"
-    : "https://cts-user-management.vercel.app/";
+    : "https://rbms-next.vercel.app/";
